1d827f1b6788823a626b7168b3da4d3b306bff37
soutammolick829 <soutammollick829@gmail.com>
2023-02-13 02:00:21 +0600

    bank commit

--- deposit.js
+++ deposit.js
@@ -1,8 +1,39 @@
+// withdraw button
 document.getElementById(`deposit-btn`).addEventListener(`click`, function(){
     const depositField = document.getElementById(`deposit-filed`);
     const depositAmount = depositField.value;
+    const newDepositAmount = parseFloat(depositAmount);
+    depositField.value = ``;
    const depositTotalElement = document.getElementById(`deposit-total`);
    const depositTotal = depositTotalElement.innerText;
-   depositTotalElement.innerText = depositAmount;
-   depositField.value = ``;
+   const previousDeposit = parseFloat(depositTotal);
+   const newDepositTotal = previousDeposit + newDepositAmount;
+   depositTotalElement.innerText = newDepositTotal;
+   
+   //    total balance section
+const balanceTotal= document.getElementById(`balance-total`);
+const previousBalance = balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(previousBalance);
+const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+balanceTotal.innerText = newBalanceTotal;
+})
+
+// withdraw button
+document.getElementById(`withdraw-btn`).addEventListener(`click`, function(){
+    const withdrawField = document.getElementById(`withdraw-filed`);
+    const withdrawAmount = withdrawField.value;
+    const newWithdrawAmount = parseFloat(withdrawAmount);
+    withdrawField.value = ``;
+    const withdrawTotalElement = document.getElementById(`Withdraw-total`);
+   const withdrawTotal = withdrawTotalElement.innerText;
+   const previousWithdraw = parseFloat(withdrawTotal);
+   const newWithdrawTotal = previousWithdraw + newWithdrawAmount;
+   withdrawTotalElement.innerText = newWithdrawTotal;
+
+   //    total balance section
+const balanceTotal= document.getElementById(`balance-total`);
+const previousBalance = balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(previousBalance);
+const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+balanceTotal.innerText = newBalanceTotal;
 })
